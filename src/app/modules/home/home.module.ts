69e7d8f909a5a0f218e0e76c2497b0e7b4c4b262
Florian Jauer <jauer@infai.org>
2018-10-11 16:55:58 +0200

    added add button

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,8 +1,13 @@
 import {NgModule} from '@angular/core';
 import {HomeComponent} from './home.component';
+import {MatButtonModule, MatIconModule} from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
     imports: [
+        MatButtonModule,
+        MatIconModule,
+        FlexLayoutModule
     ],
     declarations: [
         HomeComponent,
