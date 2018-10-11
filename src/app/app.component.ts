8fb7e94423862a2572217bd8b94831a6179a5ac0
Florian Jauer <jauer@infai.org>
2018-10-11 09:29:55 +0200

    added tlsint to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'moses-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'moses-web-ui';
+
 }
