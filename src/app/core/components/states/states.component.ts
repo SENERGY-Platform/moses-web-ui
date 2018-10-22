c9d36c9c6aa48a7042ed53754a33ae1e251b03b2
Florian Jauer <jauer@infai.org>
2018-10-22 15:01:59 +0200

    added responsive grid cols to state widgets

--- src/app/core/components/states/states.component.ts
+++ src/app/core/components/states/states.component.ts
@@ -1,42 +1,59 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, Output} from '@angular/core';
 import {RoomResponseModel} from '../../../modules/room/shared/roomResponse.model';
 import {RoomService} from '../../../modules/room/shared/room.service';
 import {WorldModel} from '../../../modules/world/shared/world.model';
 import {WorldService} from '../../../modules/world/shared/world.service';
+import {ResponsiveService} from '../../services/responsive.service';
+
+const grid = new Map([
+    ['xs', 1],
+    ['sm', 2],
+    ['md', 2],
+    ['lg', 4],
+    ['xl', 4],
+]);
 
 @Component({
-  selector: 'moses-states',
-  templateUrl: './states.component.html',
-  styleUrls: ['./states.component.css']
+    selector: 'moses-states',
+    templateUrl: './states.component.html',
+    styleUrls: ['./states.component.css']
 })
 export class StatesComponent implements OnInit {
 
-  @Input() type = '';
-  @Input() room: RoomResponseModel = {world: '', room: {id: '', name: '', devices: null}};
-  @Input() world: WorldModel = {id: '', name: '', rooms: null};
+    @Input() type = '';
+    @Input() room: RoomResponseModel = {world: '', room: {id: '', name: '', devices: null}};
+    @Input() world: WorldModel = {id: '', name: '', rooms: null};
+    @Output() gridCols = 0;
 
+    constructor(private roomService: RoomService,
+                private worldService: WorldService,
+                private responsiveService: ResponsiveService) {
+    }
 
-  constructor(private roomService: RoomService,
-              private worldService: WorldService) { }
+    ngOnInit() {
+        this.initGridCols();
+    }
 
-  ngOnInit() {
-
-  }
-
-  delete(): void {
-    switch (this.type) {
-        case 'Room': {
-            this.roomService.openRoomDeleteDialog(this.room);
-            break;
+    delete(): void {
+        switch (this.type) {
+            case 'Room': {
+                this.roomService.openRoomDeleteDialog(this.room);
+                break;
+            }
+            case 'World': {
+                console.log(this.world);
+                this.worldService.openDeleteDialog(this.world);
+                break;
+            }
         }
-        case 'World': {
-            console.log(this.world);
-            this.worldService.openDeleteDialog(this.world);
-            break;
-        }
-    };
-  }
+    }
 
+    private initGridCols(): void {
+        this.gridCols = grid.get(this.responsiveService.getActiveMqAlias()) || 0;
+        this.responsiveService.observeMqAlias().subscribe((mqAlias) => {
+            this.gridCols = grid.get(mqAlias) || 0;
+        });
+    }
 
 
 }
