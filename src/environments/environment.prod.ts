1120b161fe2d055688c67101d316c627e9c1b04b
Florian Jauer <jauer@infai.org>
2018-10-22 09:41:08 +0200

    changed slider position; added Dockerfile

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+    production: true,
+    keyCloakRealm: 'master',
+    keyCloakClientId: 'frontend',
+    zwaySwitchBinaryTypeId: 'iot#11992b78-cb4d-4bf9-b6e6-7c4522b1427f',
+    /** URLs */
+    mosesUrl: 'https://api.sepl.infai.org/moses',
+    keycloakUrl: 'https://auth.sepl.infai.org',
+    permissionSearchUrl: 'https://api.sepl.infai.org/permission/search',
 };
